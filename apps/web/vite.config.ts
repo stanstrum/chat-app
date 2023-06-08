58e94f7877c800d07be28609553301beefa12cc8
Stan Strum <stanleystrum@gmail.com>
2023-06-08 12:03:36 -0700

    More docker changes to make building and deploying work better

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,6 +1,16 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
+import { z } from "zod";
+import { processEnv } from "tools";
+
+const env = processEnv({
+	WEB_PORT: z.number().int().gte(0).lte(65535).default(5173)
+});
+
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	server: {
+		port: env.WEB_PORT
+	}
 });
